494cd23e2fac21dca73e742f579f1140eedba1f6
Shercosta <shercostagp@gmail.com>
2026-04-24 09:51:31 +0700

    feat: add image

---
+++ src/arrays/destinations.tsx
@@ -0,0 +1,11 @@
+export interface Destination {
+  id: number;
+  name: string;
+}
+
+export const destinations: Destination[] = [
+  {
+    id: 1,
+    name: "Evergreen Pine Family Lodge",
+  },
+];
