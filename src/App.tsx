04ece7f6a2f88b72940d4c6b3c7887fae14fac34
Shercosta <shercostagp@gmail.com>
2026-04-24 15:16:09 +0700

    feat: Hero component

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,7 @@ import { useState } from "react";
 import "./App.css";
 import { Navbar } from "./components/local/Navbar";
 import { destinations } from "./arrays/destinations";
-import { cn } from "./lib/utils";
-import { Button } from "./components/ui/button";
-import { ChevronRightCircle, MapPin } from "lucide-react";
+import { Hero } from "./components/local/Hero";
 
 function App() {
   const [heroDestination] = useState(
@@ -28,67 +26,7 @@ function App() {
             {/* navbar */}
             <Navbar />
             {/* content */}
-            <div className="flex flex-col flex-1">
-              {heroDestination.catchphrases ? (
-                heroDestination.catchphrases.map(
-                  (catchphrase, catchphrase_idx) => (
-                    <span
-                      key={`catchphrase-${catchphrase_idx}`}
-                      className={cn([
-                        "block leading-[0.8] text-4xl sm:text-5xl md:text-6xl lg:text-[80px]",
-                        "break-words",
-                        "text-right md:text-left",
-                      ])}
-                      style={{
-                        color: catchphrase.colorHex ?? "white",
-                        opacity: catchphrase.opacity ?? 1,
-                      }}
-                    >
-                      {catchphrase.text}
-                    </span>
-                  ),
-                )
-              ) : (
-                <>
-                  <span className="text-[100px] block leading-[0.8] text-white">
-                    Your Heaven
-                  </span>
-                  <span className="text-[100px] block leading-[0.8] text-white opacity-60">
-                    Our Trip
-                  </span>
-                </>
-              )}
-              {/* need this to take up the height of the remaining space */}
-              <div className="flex-1 grid grid-cols-1 md:grid-cols-3 items-end">
-                <div className="hidden lg:block">
-                  <div className="flex lg:max-w-[60%] text-white">
-                    {heroDestination.description}
-                  </div>
-                </div>
-                <div className="text-white hidden lg:block">
-                  <div className="flex text-lg gap-2 items-center">
-                    <MapPin /> {heroDestination.location}
-                  </div>
-                </div>
-                <div>
-                  <div className="flex flex-col gap-4 bg-black/30 backdrop-blur-sm rounded-xl p-4 text-white">
-                    <div className="text-xl flex justify-between">
-                      <div>{heroDestination.name}</div>
-                      <ChevronRightCircle className="text-white/40" />
-                    </div>
-                    <div className="flex lg:hidden text-lg gap-2 items-center text-white/60">
-                      <MapPin /> {heroDestination.location}
-                    </div>
-                    <span className="block">
-                      Rp {heroDestination.price.toLocaleString()}
-                    </span>
-                    <Button className="w-full" variant={"secondary"}>
-                      Reservasi
-                    </Button>
-                  </div>
-                </div>
-              </div>
-            </div>
+            <Hero heroDestination={heroDestination} />
           </div>
         </div>
       </div>
