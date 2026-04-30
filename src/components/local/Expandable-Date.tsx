92750dd95ddada129fef7b2f94218d0445299cd1
Shercosta <shercostagp@gmail.com>
2026-04-30 14:17:25 +0700

    fix: tighten the expendable date size to 230px

--- src/components/local/Expandable-Date.tsx
+++ src/components/local/Expandable-Date.tsx
@@ -48,7 +48,7 @@ export function ExpandableDate({ heroDestination }: HeroProps) {
             mt-3
             z-50
 
-            w-[320px]
+            w-[230px]
 
             rounded-2xl
             border border-white/10
